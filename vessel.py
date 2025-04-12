f036a03fe2603798f5dfad9e3ae50243e2d272d8
Andreas V. Jonsterhaug <andreas.vj@icloud.com>
2025-04-12 22:55:28 +0200

    vessel path follow

--- vessel.py
+++ vessel.py
@@ -2,27 +2,64 @@ import asyncio
 import websockets
 import json
 import time
+import argparse
 
 
-async def boat_client():
-    uri = "ws://10.0.0.7:8000"
+STEP_DURATION = 2       # seconds between updates
+SEGMENT_DURATION = 60   # seconds to travel from one point to the next
+STEPS_PER_SEGMENT = SEGMENT_DURATION // STEP_DURATION
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--id", type=int, required=True)
+parser.add_argument("--path", type=str, required=True)
+parser.add_argument("--host", type=str, default="localhost")
+parser.add_argument("--port", type=int, default=8000)
+
+args = parser.parse_args()
+uri = f"ws://{args.host}:{args.port}"
+
+
+async def vessel_client(vessel_id: int, path_file: str, host: str, port: str):
+    uri = f"ws://{host}:{port}"
+
+    # Load path
+    with open(path_file, 'r') as f:
+        path = json.load(f)
+
+    if len(path) < 2:
+        print("Path must contain at least 2 points.")
+        return
 
     async with websockets.connect(uri) as websocket:
-        # Identify as a boat with ID "boat_1"
         await websocket.send(json.dumps({
             "type": "vessel",
-            "id": 1,
+            "id": vessel_id,
             "timestamp": time.time()
         }))
 
+        current_index = 0
+
         while True:
-            data = {
-                "x": 42,
-                "y": 17,
-                "timestamp": time.time()
-            }
-            await websocket.send(json.dumps(data))
-            await asyncio.sleep(2)
+            # Loop back to start when reaching the end
+            start = path[current_index]
+            end = path[(current_index + 1) % len(path)]
+
+            for step in range(STEPS_PER_SEGMENT):
+                alpha = step / STEPS_PER_SEGMENT
+                lat = (1 - alpha) * start["lat"] + alpha * end["lat"]
+                lon = (1 - alpha) * start["lon"] + alpha * end["lon"]
+
+                data = {
+                    "timestamp": time.time(),
+                    "lon": lon,
+                    "lat": lat
+                }
+
+                await websocket.send(json.dumps(data))
+                await asyncio.sleep(STEP_DURATION)
+
+            current_index = (current_index + 1) % len(path)
 
 
-asyncio.run(boat_client())
+asyncio.run(vessel_client(args.id, args.path, args.host, args.port))
