a859ddc70588e7ca6e5e1528d82a7fbae0fc9b81
Andreas V. Jonsterhaug <andreas.vj@icloud.com>
2025-04-15 15:18:10 +0200

    improved handler function

--- server.py
+++ server.py
@@ -36,58 +36,86 @@ async def broadcast_updates(update_queue: asyncio.Queue):
             viewer_connections.discard(v)
 
 
+async def handle_init_message(websocket: websockets.WebSocketServerProtocol):
+    """Handles the init message and parses it as json. Also closes the connection if an error occurs"""
+    try:
+        init_message = await asyncio.wait_for(websocket.recv(), timeout=5)
+        init_data: dict = json.loads(init_message)
+        return init_data
+    except asyncio.TimeoutError:
+        print(f"Client at {websocket.remote_address} failed to send init message in time.")
+        await websocket.send(json.dumps({"error": "Timeout waiting for init message"}))
+        await websocket.close()
+        return None
+    except json.JSONDecodeError:
+        print(f"Client at {websocket.remote_address} sent invalid JSON: {init_message}")
+        await websocket.send(json.dumps({"error": "Invalid JSON"}))
+        await websocket.close()
+        return None
+
+
+async def handle_vessel_connection(websocket: websockets.WebSocketServerProtocol, update_queue: asyncio.Queue, init_data: dict):
+    """Handles a connection to a vessel"""
+    id = init_data.get("id")
+    if not id:
+        await websocket.send(json.dumps({"error": "vessel must have an ID"}))
+        return
+
+    vessel_connections.add(websocket)
+    vessel_data[id] = init_data
+    print(f"Client at {websocket.remote_address} identified as Vessel {id}")
+
+    async for message in websocket:
+        try:
+            data: dict = json.loads(message)
+            # if data.get("timestamp") > boat_data.get("timestamp"):
+            await update_queue.put((id, data))
+        except json.JSONDecodeError:
+            print(f"Invalid JSON data received from vessel {id}: {message}")
+
+
+async def handle_viewer_connection(websocket: websockets.WebSocketServerProtocol):
+    viewer_connections.add(websocket)
+    print(f"Viewer connected: {websocket.remote_address}")
+
+    # Send full boat data once
+    full_update = json.dumps({
+        "type": "full_update",
+        "vessels": vessel_data
+    })
+    await websocket.send(full_update)
+
+    try:
+        await websocket.wait_closed()
+    finally:
+        print(f"Viewver disconnected: {websocket.remote_address}")
+        viewer_connections.discard(websocket)
+
+
 async def handler(websocket: websockets.WebSocketServerProtocol, update_queue: asyncio.Queue):
     """Handler for new connections"""
     try:
-        init_message = await websocket.recv()
-        init_data: dict = json.loads(init_message)
+        print(f"Client connected: {websocket.remote_address}")
+
+        init_data = await handle_init_message(websocket)
+        if not init_data:
+            return
 
         client_type = init_data.get("type")
 
         if client_type == "vessel":
-            id = init_data.get("id")
-            if not id:
-                await websocket.send(json.dumps({"error": "vessel must have an ID"}))
-                return
-
-            vessel_connections.add(websocket)
-            vessel_data[id] = init_data
-            print(f"vessel {id} connected: {websocket.remote_address}")
-
-            async for message in websocket:
-                try:
-                    data: dict = json.loads(message)
-                    # if data.get("timestamp") > boat_data.get("timestamp"):
-                    await update_queue.put((id, data))
-                except json.JSONDecodeError:
-                    print(f"Invalid JSON data received from vessel {id}: {message}")
-
+            await handle_vessel_connection(websocket, update_queue, init_data)
 
         elif client_type == "viewer":
-            viewer_connections.add(websocket)
-            print(f"Viewer connected: {websocket.remote_address}")
-
-            # Send full boat data once
-            full_update = json.dumps({
-                "type": "full_update",
-                "vessels": vessel_data
-            })
-            await websocket.send(full_update)
-
-            try:
-                await websocket.wait_closed()
-            finally:
-                print(f"Viewver disconnected: {websocket.remote_address}")
-                viewer_connections.discard(websocket)
+            await handle_viewer_connection(websocket, update_queue)
 
         else:
             print(f"Invalid client type tried to connect: {websocket.remote_address}")
-            await websocket.send(json.dumps({"error": "Invalid client type"}))
+            await websocket.send(json.dumps({"error": f"Invalid client type: {client_type}"}))
+            websocket.close()
 
     except websockets.exceptions.ConnectionClosed:
         pass
-    except json.JSONDecodeError:
-        print("Invalid JSON from client", init_message)
     finally:
         if websocket in viewer_connections:
             viewer_connections.discard(websocket)
