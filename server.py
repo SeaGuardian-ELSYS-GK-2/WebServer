c3c9746cee94d7afa584a1fa90985334c3f724b4
Andreas V. Jonsterhaug <andreas.vj@icloud.com>
2025-04-24 16:49:01 +0200

    actually fixed crew updates

--- server.py
+++ server.py
@@ -22,9 +22,10 @@ async def broadcast_updates(update_queue: asyncio.Queue):
 
         for crew_member_id, crew_member_data in data["crewupdates"].items():
             crew_member = vessels_data[id]["crew"][crew_member_id]
-            crew_member["latitude"] = crew_member_data["latitude"]
-            crew_member["longitude"] = crew_member_data["longitude"]
             crew_member["overBoard"] = crew_member_data["overBoard"]
+            if crew_member["overBoard"]:
+                crew_member["latitude"] = crew_member_data["latitude"]
+                crew_member["longitude"] = crew_member_data["longitude"]
 
         message = json.dumps({
             "type": "vessel_update",
